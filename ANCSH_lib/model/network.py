b9966e858ae90297dbfe971ecd1bc7acd3a4a762
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2021-12-26 17:53:43 -0800

    finish the backbone PointNet++

--- ANCSH_lib/model/network.py
+++ ANCSH_lib/model/network.py
@@ -1,1 +1,9 @@
 import torch
+import torch.nn as nn
+from backbone import PointNet2
+
+class ANCSH(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Define the shared PN++
+        self.backbone = PointNet2()
