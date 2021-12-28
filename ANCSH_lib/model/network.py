e693c8fda2e11ffddaedbce5fe935e9908eb9668
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2021-12-27 20:08:01 -0800

    finish the loss functions

--- ANCSH_lib/model/network.py
+++ ANCSH_lib/model/network.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from backbone import PointNet2
+import loss
 
 class ANCSH(nn.Module):
     def __init__(self, num_parts):
@@ -18,8 +19,6 @@ class ANCSH(nn.Module):
         # NAOCS scale and translation
         self.scale_layer = nn.Conv1d(128, 1*num_parts, kernel_size=1, padding='valid')
         self.trans_layer = nn.Conv2d(128, 3*num_parts, kernel_size=1, padding='valid')
-        # Confidence
-        self.conf_layer = nn.Conv1d(128, 1, kernel_size=1, padding='valid')
         # Joint parameters
         self.joint_feature_layer = nn.Sequential(
             nn.Conv1d(128, 128, kernel_size=1, padding='valid', bias=False),
@@ -44,7 +43,6 @@ class ANCSH(nn.Module):
         pred_npcs_per_point = self.npcs_layer(features)
         pred_scale_per_point = self.scale_layer(features)
         pred_trans_per_point = self.trans_layer(features)
-        pred_conf_per_point = self.conf_layer(features)
 
         joint_features = self.joint_feature_layer(features)
         pred_axis_per_point = self.axis_layer(joint_features)
@@ -57,7 +55,6 @@ class ANCSH(nn.Module):
         pred_trans_per_point = F.tanh(pred_trans_per_point)
         
         pred_seg_per_point = F.softmax(pred_seg_per_point, dim=2)
-        pred_conf_per_point = F.sigmoid(pred_conf_per_point)
         pred_npcs_per_point = F.sigmoid(pred_npcs_per_point)
 
         pred_heatmap_per_point = F.sigmoid(pred_heatmap_per_point)
@@ -71,16 +68,54 @@ class ANCSH(nn.Module):
 
         pred = {
             'seg_per_point': pred_seg_per_point,
-            'NPCS_per_point': pred_npcs_per_point,
-            'conf_per_point': pred_conf_per_point,
+            'npcs_per_point': pred_npcs_per_point,
             'heatmap_per_point': pred_heatmap_per_point,
-            'unit_vec_per_point': pred_unitvec_per_point,
+            'unitvec_per_point': pred_unitvec_per_point,
             'axis_per_point': pred_axis_per_point,
             'joint_cls_per_point': pred_joint_cls_per_point,
             'scale_per_point': pred_scale_per_point,
             'trans_per_point': pred_trans_per_point,
-            'NAOCS_per_point': pred_naocs_per_point
+            'naocs_per_point': pred_naocs_per_point
         }
 
+        return pred
 
+    def losses(self, pred, gt):
+        # The returned loss is a value
+        num_parts = pred['seg_per_point'].shape[2]
+        # Convert the gt['seg_per_point'] into gt_seg_onehot B*N*K
+        gt_seg_onehot = F.one_hot(gt['seg_per_point'], num_class=num_parts)
+        # pred['seg_per_point']: B*N*K, gt_seg_onehot: B*N*K
+        seg_loss = loss.compute_miou_loss(pred['seg_per_point'], gt_seg_onehot)
+        # pred['npcs_per_point']: B*N*3K, gt['npcs_per_point']: B*N*3, gt_seg_onehot: B*N*K
+        npcs_loss = loss.compute_coorindate_loss(pred['npcs_per_point'], gt['npcs_per_point'], num_parts=num_parts, gt_seg_onehot=gt_seg_onehot)
+        # pred['naocs_per_point']: B*N*3K, gt['naocs_per_point']: B*N*3, gt_seg_onehot: B*N*K
+        naocs_loss = loss.compute_coorindate_loss(pred['naocs_per_point'], gt['naocs_per_point'], num_parts=num_parts, gt_seg_onehot=gt_seg_onehot)
         
+        # Get the useful joint mask, gt['joint_cls_per_point'] == 0 means that that point doesn't have a corresponding joint
+        # B*N
+        gt_joint_mask = (gt['joint_cls_per_point'] > 0).float()
+        # pred['heatmap_per_point']: B*N*1, gt['heatmap_per_point']: B*N, gt_joint_mask: B*N
+        heatmap_loss = loss.compute_vect_loss(pred['heatmap_per_point'], gt['heatmap_per_point'], mask=gt_joint_mask)
+        # pred['unitvec_per_point']: B*N*3, gt['unitvec_per_point']: B*N*3, gt_joint_mask: B*N
+        unitvec_loss = loss.compute_vect_loss(pred['unitvec_per_point'], gt['unitvec_per_point'], mask=gt_joint_mask)
+        # pred['axis_per_point]: B*N*3, gt['axis_per_point']: B*N*3, gt_joint_mask: B*N
+        axis_loss = loss.compute_vect_loss(pred['axis_per_point'], gt['axis_per_point'], mask=gt_joint_mask)
+
+        # Conver the gt['joint_cls_per_point'] into gt_joint_cls_onehot B*N*K
+        gt_joint_cls_onehot = F.one_hot(gt['joint_cls_per_point'], num_class=num_parts)
+        joint_loss = loss.compute_miou_loss(pred['joint_cls_per_point'], gt_joint_cls_onehot)
+
+        loss_dict = {
+            'seg_loss': seg_loss,
+            'npcs_loss': npcs_loss,
+            'naocs_loss': naocs_loss,
+            'heatmap_loss': heatmap_loss,
+            'unitvec_loss': unitvec_loss,
+            'axis_loss': axis_loss,
+            'joint_loss': joint_loss
+        }
+
+        return loss_dict
+
+
