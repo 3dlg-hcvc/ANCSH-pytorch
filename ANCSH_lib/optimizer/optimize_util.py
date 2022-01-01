853b37089426079c5eb0e1567a57ce80b3b5c014
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2021-12-31 21:10:20 -0800

    finish algorithms for the optimization process

--- ANCSH_lib/optimizer/optimize_util.py
+++ ANCSH_lib/optimizer/optimize_util.py
@@ -1,19 +1,218 @@
 import numpy as np
+from scipy.spatial.transform import Rotation as srot
+from scipy.optimize import least_squares
 
-def ransac(dataset, model_estimator, model_verifier, inlier_th, niter=10000):
+def ransac(dataset, model_estimator, model_verifier, inlier_th, niter, joint_type):
     best_model = None
     best_score = -np.inf
     best_inliers = None
     for i in range(niter):
-        cur_model = model_estimator(dataset)
+        cur_model = model_estimator(dataset, joint_type)
         cur_score, cur_inliers = model_verifier(dataset, cur_model, inlier_th)
         if cur_score > best_score:
             best_model = cur_model
             best_inliers = cur_inliers
             best_score = cur_score
-    best_model = model_estimator(dataset, best_inliers)
+    best_model = model_estimator(dataset, joint_type, best_inliers)
     return best_model, best_inliers
 
+def joint_transformation_estimator(dataset, joint_type="revolute", best_inliers=None):
+    # dataset: dict, fields include source0, target0, nsource0,
+    #     source1, target1, nsource1, joint_direction
+    if best_inliers is None:
+        sample_idx0 = np.random.randint(dataset["nsource0"], size=3)
+        sample_idx1 = np.random.randint(dataset["nsource1"], size=3)
+    else:
+        sample_idx0 = best_inliers[0]
+        sample_idx1 = best_inliers[1]
+
+    source0 = dataset["source0"][sample_idx0, :]
+    target0 = dataset["target0"][sample_idx0, :]
+    source1 = dataset["source1"][sample_idx1, :]
+    target1 = dataset["target1"][sample_idx1, :]
+    # prescaling and centering
+    scale0 = scale_pts(source0, target0)
+    scale1 = scale_pts(source1, target1)
+    scale0_inv = scale_pts(target0, source0)  # check if could simply take reciprocal
+    scale1_inv = scale_pts(target1, source1)
+
+    target0_scaled_centered = scale0_inv * target0
+    target0_scaled_centered -= np.mean(target0_scaled_centered, 0, keepdims=True)
+    source0_centered = source0 - np.mean(source0, 0, keepdims=True)
+
+    target1_scaled_centered = scale1_inv * target1
+    target1_scaled_centered -= np.mean(target1_scaled_centered, 0, keepdims=True)
+    source1_centered = source1 - np.mean(source1, 0, keepdims=True)
+
+    joint_points0 = np.ones_like(
+        np.linspace(0, 1, num=np.min((source0.shape[0], source1.shape[0])) + 1)[
+            1:
+        ].reshape((-1, 1))
+    ) * dataset["joint_direction"].reshape((1, 3))
+
+    R0 = rotate_pts(source0_centered, target0_scaled_centered)
+    R1 = rotate_pts(source1_centered, target1_scaled_centered)
+
+    T0 = np.mean(target0_scaled_centered.T - np.matmul(R0, source0_centered.T), 1)
+    T1 = np.mean(target0_scaled_centered.T - np.matmul(R0, source0_centered.T), 1)
+
+    rotvec0 = srot.from_dcm(R0).as_rotvec()
+    rotvec1 = srot.from_dcm(R1).as_rotvec()
+    # print('initialize rotvec0 vs rotvec1: \n', rotvec0, rotvec1)
+    if joint_type == 0:
+        # 0 represents primatic
+        res = least_squares(
+            objective_eval_t,
+            np.hstack((T0, T1)),
+            verbose=0,
+            ftol=1e-4,
+            method="lm",
+            args=(
+                source0_centered,
+                target0_scaled_centered,
+                source1_centered,
+                target1_scaled_centered,
+                joint_points0,
+                R0,
+                R1,
+                1.0,
+                1.0,
+                False,
+            ),
+        )
+    elif joint_type == 1:
+        # 1 represents revolute
+        res = least_squares(
+            objective_eval_r,
+            np.hstack((rotvec0, rotvec1)),
+            verbose=0,
+            ftol=1e-4,
+            method="lm",
+            args=(
+                source0_centered,
+                target0_scaled_centered,
+                source1_centered,
+                target1_scaled_centered,
+                joint_points0,
+                False,
+            ),
+        )
+    R0 = srot.from_rotvec(res.x[:3]).as_dcm()
+    R1 = srot.from_rotvec(res.x[3:]).as_dcm()
+
+    translation0 = np.mean(target0.T - scale0 * np.matmul(R0, source0.T), 1)
+    translation1 = np.mean(target1.T - scale1 * np.matmul(R1, source1.T), 1)
+
+    jtrans = dict()
+    jtrans["rotation0"] = R0
+    jtrans["scale0"] = scale0
+    jtrans["translation0"] = translation0
+    jtrans["rotation1"] = R1
+    jtrans["scale1"] = scale1
+    jtrans["translation1"] = translation1
+    return jtrans
+
+
+def joint_transformation_verifier(dataset, model, inlier_th):
+    # dataset: dict, fields include source, target, nsource, ntarget
+    # model: dict, fields include rotation, scale, translation
+    res0 = (
+        dataset["target0"].T
+        - model["scale0"] * np.matmul(model["rotation0"], dataset["source0"].T)
+        - model["translation0"].reshape((3, 1))
+    )
+    inliers0 = np.sqrt(np.sum(res0 ** 2, 0)) < inlier_th
+    res1 = (
+        dataset["target1"].T
+        - model["scale1"] * np.matmul(model["rotation1"], dataset["source1"].T)
+        - model["translation1"].reshape((3, 1))
+    )
+    inliers1 = np.sqrt(np.sum(res1 ** 2, 0)) < inlier_th
+    score = (np.sum(inliers0) / res0.shape[0] + np.sum(inliers1) / res1.shape[0]) / 2
+    return score, [inliers0, inliers1]
+
+def objective_eval_r(params, x0, y0, x1, y1, joints, isweight=True):
+    # params: [:3] R0, [3:] R1
+    # x0: N x 3, y0: N x 3, x1: M x 3, y1: M x 3, R0: 1 x 3, R1: 1 x 3, joints: K x 3
+    rotvec0 = params[:3].reshape((1, 3))
+    rotvec1 = params[3:].reshape((1, 3))
+    res0 = y0 - rotate_points_with_rotvec(x0, rotvec0)
+    res1 = y1 - rotate_points_with_rotvec(x1, rotvec1)
+    res_joint = rotate_points_with_rotvec(joints, rotvec0) - rotate_points_with_rotvec(
+        joints, rotvec1
+    )
+    if isweight:
+        res0 /= x0.shape[0]
+        res1 /= x1.shape[0]
+        res_joint /= joints.shape[0]
+    return np.concatenate((res0, res1, res_joint), 0).ravel()
+
+def objective_eval_t(
+    params, x0, y0, x1, y1, joints, R0, R1, scale0, scale1, isweight=True
+):
+    # params: [0:3] t0, [3:6] t1;
+    # joints: K * 3
+    # rotvec0, rotvec1, scale0, scale1 solved from previous steps
+    R = R0
+    transvec0 = params[0:3].reshape((1, 3))
+    transvec1 = params[3:6].reshape((1, 3))
+    res0 = y0 - scale0 * np.matmul(x0, R0.T) - transvec0
+    res1 = y1 - scale1 * np.matmul(x1, R1.T) - transvec1
+    rot_u = np.matmul(joints, R.T)[0]
+    delta_trans = transvec0 - transvec1
+    cross_mat = np.array(
+        [[0, -rot_u[2], rot_u[1]], [rot_u[2], 0, -rot_u[0]], [-rot_u[1], rot_u[0], 0]]
+    )
+    res2 = np.matmul(delta_trans, cross_mat.T).reshape(1, 3)
+    # np.linspace(0, 1, num = np.min((x0.shape[0], x1.shape[0]))+1 )[1:].reshape((-1, 1))
+    res2 = np.ones((np.min((x0.shape[0], x1.shape[0])), 1)) * res2
+    if isweight:
+        res0 /= x0.shape[0]
+        res1 /= x1.shape[0]
+        res2 /= res2.shape[0]
+    return np.concatenate((res0, res1, res2), 0).ravel()
+
+def scale_pts(source, target):
+    '''
+    func: compute scaling factor between source: [N x 3], target: [N x 3]
+    '''
+    pdist_s = source.reshape(source.shape[0], 1, 3) - source.reshape(1, source.shape[0], 3)
+    A = np.sqrt(np.sum(pdist_s**2, 2)).reshape(-1)
+    pdist_t = target.reshape(target.shape[0], 1, 3) - target.reshape(1, target.shape[0], 3)
+    b = np.sqrt(np.sum(pdist_t**2, 2)).reshape(-1)
+    scale = np.dot(A, b) / (np.dot(A, A)+1e-6)
+    return scale
+
+def rotate_pts(source, target):
+    '''
+    func: compute rotation between source: [N x 3], target: [N x 3]
+    '''
+    # pre-centering
+    source = source - np.mean(source, 0, keepdims=True)
+    target = target - np.mean(target, 0, keepdims=True)
+    M = np.matmul(target.T, source)
+    U, D, Vh = np.linalg.svd(M, full_matrices=True)
+    d = (np.linalg.det(U) * np.linalg.det(Vh)) < 0.0
+    if d:
+        D[-1] = -D[-1]
+        U[:, -1] = -U[:, -1]
+    R = np.matmul(U, Vh)
+    return R
+
+def rotate_points_with_rotvec(points, rot_vecs):
+    """Rotate points by given rotation vectors.
+
+    Rodrigues' rotation formula is used.
+    """
+    theta = np.linalg.norm(rot_vecs, axis=1)[:, np.newaxis]
+    with np.errstate(invalid='ignore'):
+        v = rot_vecs / theta
+        v = np.nan_to_num(v)
+    dot = np.sum(points * v, axis=1)[:, np.newaxis]
+    cos_theta = np.cos(theta)
+    sin_theta = np.sin(theta)
+
+    return cos_theta * points + sin_theta * np.cross(v, points) + dot * (1 - cos_theta) * v
 
 def optimize_with_kinematic(ins_ancsh, ins_npcs, num_parts, niter, choose_threshold):
     camcs_per_point = ins_ancsh["camcs_per_point"]
@@ -51,12 +250,14 @@ def optimize_with_kinematic(ins_ancsh, ins_npcs, num_parts, niter, choose_thresh
         # Get the constrained joint info
         data["joint_direction"] = np.median(pred_axis_per_point[jointIndex[i-1]], axis=0)
 
+        assert joint_type >= 0
+
         best_model, best_inliers = ransac(
             data,
             joint_transformation_estimator,
             joint_transformation_verifier,
             choose_threshold,
             niter,
-            joint_type,
+            joint_type[i],
         )
     
--- README.md
+++ README.md
@@ -22,6 +22,7 @@ pip install -e .
 pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 pip install "git+git://github.com/erikwijmans/Pointnet2_PyTorch.git#egg=pointnet2_ops&subdirectory=pointnet2_ops_lib"
 pip install h5py
+pip install scipy
 ```
 
 ## Running Experiments
