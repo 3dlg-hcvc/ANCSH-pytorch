b00ba47bd8363daeca8921797b22f19ef7b63ced
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2022-01-01 21:04:18 -0800

    finish the evaluation metric in compute_miou.py

--- ANCSH_lib/evaluator/evaluator.py
+++ ANCSH_lib/evaluator/evaluator.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+import itertools
 
 
 def get_3d_bbox(scale, shift=0):
@@ -29,6 +30,37 @@ def get_3d_bbox(scale, shift=0):
     )
     return bbox_3d
 
+def pts_inside_box(pts, bbox):
+    # pts: N x 3
+    # bbox: 8 x 3 (-1, 1, 1), (1, 1, 1), (1, -1, 1), (-1, -1, 1), (-1, 1, -1), (1, 1, -1), (1, -1, -1), (-1, -1, -1)
+    u1 = bbox[5, :] - bbox[4, :]
+    u2 = bbox[7, :] - bbox[4, :]
+    u3 = bbox[0, :] - bbox[4, :]
+
+    up = pts - np.reshape(bbox[4, :], (1, 3))
+    p1 = np.matmul(up, u1.reshape((3, 1)))
+    p2 = np.matmul(up, u2.reshape((3, 1)))
+    p3 = np.matmul(up, u3.reshape((3, 1)))
+    p1 = np.logical_and(p1>0, p1<np.dot(u1, u1))
+    p2 = np.logical_and(p2>0, p2<np.dot(u2, u2))
+    p3 = np.logical_and(p3>0, p3<np.dot(u3, u3))
+    return np.logical_and(np.logical_and(p1, p2), p3)
+
+def iou_3d(bbox1, bbox2, nres=50):
+    bmin = np.min(np.concatenate((bbox1, bbox2), 0), 0)
+    bmax = np.max(np.concatenate((bbox1, bbox2), 0), 0)
+    xs = np.linspace(bmin[0], bmax[0], nres)
+    ys = np.linspace(bmin[1], bmax[1], nres)
+    zs = np.linspace(bmin[2], bmax[2], nres)
+    pts = np.array([x for x in itertools.product(xs, ys, zs)])
+    flag1 = pts_inside_box(pts, bbox1)
+    flag2 = pts_inside_box(pts, bbox2)
+    intersect = np.sum(np.logical_and(flag1, flag2))
+    union = np.sum(np.logical_or(flag1, flag2))
+    if union==0:
+        return 1
+    else:
+        return intersect/float(union)
 
 class ANCSHEvaluator:
     def __init__(self, cfg, combined_results_path):
@@ -56,7 +88,7 @@ class ANCSHEvaluator:
             gt_norm_factors = ins_combined["gt_norm_factors"]
             gt_corners = ins_combined["gt_corners"]
 
-            result = {"pose_err_scale": [], "pose_err_volume": []}
+            result = {"pose_err_scale": [], "pose_err_volume": [], "cam_3dbbx_iou": [], "gt_cam_3dbbx": [], "pred_cam_3dbbx": []}
             for partIndex in range(num_parts):
                 norm_factor = gt_norm_factors[partIndex]
                 corner = gt_corners[partIndex]
@@ -109,7 +141,16 @@ class ANCSHEvaluator:
                 # https://github.com/dragonlong/articulated-pose/blob/master/evaluation/eval_pose_err.py#L320
 
                 # Calculatet the 3diou for each part
-                
+                gt_scaled_3dbbx = gt_3dbbx * gt_npcs_scale[partIndex]
+                pred_scaled_3dbbx = pred_3dbbx * pred_npcs_scale[partIndex]
+                gt_cam_3dbbx = np.dot(gt_npcs_rt[:3, :3], gt_scaled_3dbbx.T).T + gt_npcs_rt[:3, 3].T
+                pred_cam_3dbbx = np.dot(pred_npcs_rt[:3, :3], pred_scaled_3dbbx.T).T + pred_npcs_rt[:3, 3].T
+                cam_3dbbx_iou = iou_3d(gt_cam_3dbbx, pred_cam_3dbbx)
+                result["gt_cam_3dbbx"].append(gt_cam_3dbbx)
+                result["pred_cam_3dbbx"].append(pred_cam_3dbbx)
+                result["cam_3dbbx_iou"].append(cam_3dbbx_iou)
+
+                # Calculate the evaluation metric for the joints
 
             results.append(result)
 
