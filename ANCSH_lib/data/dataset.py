a7de8d0a7406104c3a5450c2180829266b62d098
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2022-01-02 14:46:12 -0800

    finish the dataset code

--- ANCSH_lib/data/dataset.py
+++ ANCSH_lib/data/dataset.py
@@ -3,14 +3,25 @@ import h5py
 import torch
 
 class ANCSHDataset(Dataset):
-    def __init__(self, gt_path):
-        # self.h5file = h5py.File(gt_path)
-        self.x = torch.rand(32, 1024, 3)
+    def __init__(self, data_path):
+        self.f_data = h5py.File(data_path)
+        self.instances = sorted(self.f_data)
     
     def __len__(self):
-        return self.x.shape[0]
+        return len(self.instances)
 
     def __getitem__(self, index):
-        camcs_per_point = self.x[index]
-        gt_dict = {"filename": "TEST", "value": torch.rand(2)}
+        ins = self.f_data[self.instances[index]]
+        # Get the camcs_per_point
+        camcs_per_point = ins["camcs_per_point"]
+        # Get the joint type
+        joint_type = ins["joint_type"]
+        # Get all other items 
+        gt_dict = {}
+        for k, v in ins.items():
+            if k == "camcs_per_point" or k == "joint_type":
+                continue
+            else:
+                gt_dict[k] = v
+        
         return (camcs_per_point, gt_dict, joint_type)
