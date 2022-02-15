17712dcfa587cd09021333290263b57337f3e658
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2022-02-14 19:43:43 -0800

    update the visualization scripts

--- visualize_process.py
+++ visualize_process.py
@@ -1,9 +1,19 @@
 import h5py
 import os
-from tools.visualization import OptimizerVisualizer
+from tools.visualization import ANCSHVisualizer
+from enum import Enum
+
+class NetworkType(Enum):
+    ANCSH = 'ANCSH'
+    NPCS = 'NPCS'
+
+h5_output_path = "/local-scratch/localhome/hja40/Desktop/Research/proj-motionnet/ANCSH-pytorch/data/eyeglasses_ancsh/test.h5"
+visualization_path = "/local-scratch/localhome/hja40/Desktop/Research/proj-motionnet/ANCSH-pytorch/visualization"
 
 if __name__ == "__main__":
-    with h5py.File(, "r") as h5file:
-        visualizer = OptimizerVisualizer(h5file)
-        export_dir = os.path.join()
-        visualizer.render(show=False, export=export_dir, export_mesh=False)
+    with h5py.File(h5_output_path, 'r') as h5file:
+        visualizer = ANCSHVisualizer(h5file, NetworkType.ANCSH, gt=True, sampling=20)
+        visualizer.point_size = 5
+        visualizer.arrow_sampling = 10
+        visualizer.prefix = ''
+        visualizer.render(show=False, export=visualization_path, export_mesh=True)
