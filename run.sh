fb273fb2af27a86c1e05ab9898f54ecf81806a3c
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2022-03-18 13:54:36 -0700

    clean code, ready for release

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PROJ_DIR=/home/hanxiao/projects/rrg-msavva/hanxiao/proj-motionnet/ANCSH-pytorch
-sbatch --exclude=cdr26,cdr27,cdr28,cdr29,cdr30,cdr31,cdr32,cdr33,cdr34,cdr35,cdr40,cdr104,cdr111,cdr905,cdr922,cdr199 $PROJ_DIR/train_script_ancsh.sh
-sbatch --exclude=cdr26,cdr27,cdr28,cdr29,cdr30,cdr31,cdr32,cdr33,cdr34,cdr35,cdr40,cdr104,cdr111,cdr905,cdr922,cdr199 $PROJ_DIR/train_script_npcs.sh
